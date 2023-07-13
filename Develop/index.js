c3e5f1ad5b5d9e5ccfff765fe50352f38247055e
Madeline Estes <maddiegirl.360@gmail.com>
2023-07-13 11:09:02 -0700

    save changes to starter code

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,90 @@
 // TODO: Include packages needed for this application
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const inquirer = require("inquirer")
+const fs = require("fs")
+const generateMarkdown = require("./utils/generateMarkdown")
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of the Application?',
+    },  
+
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Describe what the purpose and function of the application.',
+    },
+
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Describe any extra steps needed to install the application.',
+    },
+
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Briefly describe how to use the application.',
+    },
+
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Provide guidelines for others to contribute to the application.',
+    },
+    
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Describe how to test the application.',
+    },
+
+    {
+        type: 'input',
+        name: 'credits',
+        message: 'Provide any credits to contributors.',
+    },
+
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Select a license from the following list.',
+        choices: ['Apache 2.0', 'ISC', 'MIT', 'MPL 2.0', 'ODC BY', 'This application does not have a license']
+    },
+
+    {
+        type: 'input',
+        name: 'GitHub',
+        message: 'Provide your GitHub username.',
+    },
+
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Provide your email address.',
+    },
+  
+  ];
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {inquirer
+  .prompt(questions) 
+  .then(function(data) {
+    console.log(data)
+
+  fs.writeFile("draftREADME.md", generateMarkdown (data), function (err)
+      {if (err) 
+        {console.log(err)
+       } else {
+          console.log('Success!')
+        }
+      }
+      );
+  });}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,116 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === 'MIT') {
+    return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]';
+  } else if (license === 'MPL 2.0') {
+    return '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)]';
+  
+  } else if (license === 'ODC BY') {
+    return '[![License: Open Data Commons Attribution](https://img.shields.io/badge/License-ODC_BY-brightgreen.svg)]';
+  } else if (license === 'ISC') {
+    return '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)]';
+  
+  } else if (license === 'Apache 2.0') {  
+    return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)]';
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'MIT') {
+  return '(https://opensource.org/licenses/MIT)';
+
+} else if (license === 'MPL 2.0') {
+  return '(https://opensource.org/licenses/MPL-2.0)';
+
+} else if (license === 'ODC BY') {
+  return '(https://opendatacommons.org/licenses/by/)'
+} else if (license === 'ISC') {
+  return '(https://opensource.org/licenses/ISC)';
+
+} else if (license === 'Apache 2.0') {  
+  return '(https://opensource.org/licenses/Apache-2.0)';
+} else {
+  return '';
+}}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
+function renderLicenseSection(license) {
+  if (license === 'This application does not have a license') {
+    return 'This project does not have a license.'
+  } else {
+  return `${renderLicenseBadge(license)}${renderLicenseLink(license)}
+  
+  This project is licensed under the terms of the ${license} license.`;
+}
+}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+ 
 
+  # ${data.name}
+ 
+  ${renderLicenseBadge(data.license)}${renderLicenseLink(data.license)}
+ 
+  ## Table of Contents
+  
+  [Description](#description)
+
+  [Installation](#installation)
+  
+  [Usage](#usage)
+
+  [Contribute](#contribute)
+
+  [Test](#test)
+  
+  [Credits](#credits)
+  
+  [License](#license)
+
+  [Questions](#questions)
+  
+  ## Description
+  ${data.description}
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ![alt text](./screencap1.PNG)
+  ![alt text](./screencap2.PNG)
+  
+  ## Contribute
+  ${data.contribution}
+
+  [GitHub Repo: (https://github.com/)]
+
+  ## Test
+  ${data.test}
+  
+  ## Credits
+  ${data.credits}
+
+  ## License
+  
+  ${renderLicenseSection(data.license)}
+
+  ## Questions?
+
+  Contact me at:
+
+  GitHub: https://github.com/${data.GitHub}
+  
+  email: ${data.email}
+  
 `;
 }
 
